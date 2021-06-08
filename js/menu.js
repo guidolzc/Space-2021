e004afbfa2f863c066936fcd02b02e5284ba9f4e
guidolzc <pcigfernandez@gmail.com>
2021-06-08 12:48:04 -0700

    space

---
+++ js/menu.js
@@ -0,0 +1,6 @@
+var btnMenu = document.getElementById('menu-icon1');
+var nav = document.getElementById('nav1');
+
+ btnMenu.addEventListener('click', function(){
+     nav.classList.toggle('mostrar');
+ })
